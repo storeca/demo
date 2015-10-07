66081792e277343b1ac5efebd56f5f172e50e67f
fb291179 <fb291179@C39435.ens.uqam.ca>
2015-10-07 10:56:46 -0300

    commit initial

---
+++ src/demogit/Demogit.java
@@ -0,0 +1,17 @@
+
+package demogit;
+
+/**
+ *
+ * @author fb291179
+ */
+public class Demogit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
