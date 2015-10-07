776ba8c592f4e28327aa76d7c5ece382ab615a74
fb291179 <fb291179@C39435.ens.uqam.ca>
2015-10-07 11:33:40 -0300

    ------------------

--- src/demogit/Demogit.java
+++ src/demogit/Demogit.java
@@ -11,7 +11,7 @@ public class Demogit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Nahed Boudraa");
+        System.out.println("==============");
     }
     
 }
